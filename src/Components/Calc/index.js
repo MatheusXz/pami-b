9517167416e88c29448dd620daf80b0b4bb2e291
MatheusXz <matheus000000018@gmail.com>
2023-06-15 22:31:51 -0300

    up

--- src/Components/Calc/index.js
+++ src/Components/Calc/index.js
@@ -5,15 +5,23 @@ import Constants from 'expo-constants';
 // CALCULADORA COMUM
 
 const Calc = () => {
-    const [resultado, setResult] = useState(0);
-    const [value, setValue] = useState(0);
+    const [resultadofim, setResult] = useState(0);
+    const [value, setValue] = useState();
     const [numPress, setNumPress] = useState(0);
     const [sinPress, setSinPress] = useState(0);
     const [separacaoArray, setArraySeparacao] = useState();
 
-
+    const sinais = [
+        '-', '+', '*', '/'
+    ];
+    const numbers = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'
+    ]
     const valuePress = (number) => {
-
+        
+        if (number === '/') {
+            
+        }
         const newSinPress = value + number.toString();
         setValue(newSinPress);
     };
@@ -22,247 +30,237 @@ const Calc = () => {
         let valorSplit = 0;
         let numberConcatenado = 0;
         let arrayFinal = [];
-        const sinais = [
-            '-', '+', '*', '/'
-        ];
-        const numbers = [
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-        ]
+        let resultado = 0;
+        
         valorSplit = array.split('')
         for (let i = 0; i < array.length; i++) {
             if (numbers.includes(valorSplit[i])) {
                 numberConcatenado += valorSplit[i]
-
-                console.log(numberConcatenado)
             } else if (sinais.includes(valorSplit[i])) {
                 arrayFinal.push(numberConcatenado)
                 numberConcatenado = '';
                 arrayFinal.push(valorSplit[i])
-
-                console.log(arrayFinal)
-            } else {
-
-
-            // }else if (botaoApertado == "=") {
-
-            //     //um for que percore todo o arrey executando as multiplicação e divisão
-            //     for (contador = 0; contador < array.length; contador++) {
-            //         //achou multiplicação
-            //         if (array[contador] == '*') {
-            //             //pega o termo anterior ao sinal e posterior ao sinal(dois numero)
-            //             num1 = parseFloat(array[contador - 1]);
-            //             num2 = parseFloat(array[contador + 1]);
-            //             //executa a multiplicação
-            //             resultado = num1 * num2;
-            //             // apaga os 3 termos do array referente a operação(n1,n2 e o sinal)
-            //             array.splice(contador - 1, 3);
-            //             //adiciona o resultado no lugar dos outros 3 termos
-            //             array.splice(contador - 1, 0, resultado);
-            //         }
-            //         if (array[contador] == '/') {
-            //             num1 = parseFloat(array[contador - 1]);
-            //             num2 = parseFloat(array[contador + 1]);
-            //             resultado = num1 * num2;
-            //             array.splice(contador - 1, 3);
-            //             array.splice(contador - 1, 0, resultado);
-            //         }
-            //     }
-            //     for (contador = 0; contador < array.length; contador++) {
-            //         if (array[contador] == '+') {
-            //             num1 = parseFloat(array[contador - 1]);
-            //             num2 = parseFloat(array[contador + 1]);
-            //             resultado = num1 * num2;
-            //             array.splice(contador - 1, 3);
-            //             array.splice(contador - 1, 0, resultado);
-            //         }
-            //         if (array[contador] == '-') {
-            //             num1 = parseFloat(array[contador - 1]);
-            //             num2 = parseFloat(array[contador + 1]);
-            //             resultado = num1 * num2;
-            //             array.splice(contador - 1, 3);
-            //             array.splice(contador - 1, 0, resultado);
-            //         }
-            //     }
-            // }
-
-
-
-
+            }
         }
+        arrayFinal.push(numberConcatenado)
+        for (contador = 0; contador < arrayFinal.length; contador++) {
+            //achou multiplicação
+            if (arrayFinal[contador] == '*') {
+                //pega o termo anterior ao sinal e posterior ao sinal(dois numero)
+                num1 = parseFloat(arrayFinal[contador - 1]);
+                num2 = parseFloat(arrayFinal[contador + 1]);
+                //executa a multiplicação
+                resultado = num1 * num2;
+                // apaga os 3 termos do arrayFinal referente a operação(n1,n2 e o sinal)
+                arrayFinal.splice(contador - 1, 3);
+                //adiciona o resultado no lugar dos outros 3 termos
+                arrayFinal.splice(contador - 1, 0, resultado);
+                contador--;
+            }
+            if (arrayFinal[contador] == '/') {
+                num1 = parseFloat(arrayFinal[contador - 1]);
+                num2 = parseFloat(arrayFinal[contador + 1]);
+                resultado = num1 / num2;
+                arrayFinal.splice(contador - 1, 3);
+                arrayFinal.splice(contador - 1, 0, resultado);
+                contador--;
 
+
+            }
+
+
+            if (sinais.includes(array)) {
+                valorFim = array.split('')
+            }
+            
+        }
+        for (contador = 0; contador < arrayFinal.length; contador++) {
+            if (arrayFinal[contador] == '+') {
+                num1 = parseFloat(arrayFinal[contador - 1]);
+                num2 = parseFloat(arrayFinal[contador + 1]);
+                resultado = num1 + num2;
+                arrayFinal.splice(contador - 1, 3);
+                arrayFinal.splice(contador - 1, 0, resultado);
+                contador--;
+
+            }
+            if (arrayFinal[contador] == '-') {
+                num1 = parseFloat(arrayFinal[contador - 1]);
+                num2 = parseFloat(arrayFinal[contador + 1]);
+                resultado = num1 - num2;
+                arrayFinal.splice(contador - 1, 3);
+                arrayFinal.splice(contador - 1, 0, resultado);
+                contador--;
+
+            }
+        }
+        setValue(arrayFinal)
     }
 
 
-    if (sinais.includes(array)) {
-        valorFim = array.split('')
-    }
-    // console.log(numberConcatenado)
-
-}
 
 
 
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerResult}>
+                    {value}
+                </Text>
+            </View>
+            <View style={styles.contentsButtons}>
 
-return (
-    <View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.headerResult}>
-                {value}
-            </Text>
+                <TouchableOpacity
+
+
+                    onPress={() => {
+                        setValue('0')
+                    }}
+
+                    style={[styles.button, styles.buttonAC]}
+                >
+                    <Text>AC</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress('/')
+                    }
+                    }
+                >
+                    <Text>/</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.contentsButtons}>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(7)
+                    }
+                    }
+                >
+
+                    <Text>7</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(8)
+                    }
+                    }
+                >
+                    <Text>8</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(9)
+                    }
+                    }
+                >
+                    <Text>9</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress('*')
+                    }
+                    }
+                >
+                    <Text>*</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.contentsButtons}>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(4)
+                    }
+                    }
+                >
+                    <Text>4</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(5)
+                    }
+                    }
+                >
+                    <Text>5</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(6)
+                    }
+                    }
+                >
+                    <Text>6</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress('-')
+                    }
+                    }
+                >
+                    <Text>-</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.contentsButtons}>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(1)
+                    }
+                    }
+                >
+                    <Text>1</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(2)
+                    }
+                    }
+                >
+                    <Text>2</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress(3)
+                    }
+                    }
+                >
+                    <Text>3</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress('+')
+                    }
+                    }
+                >
+                    <Text>+</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.contentsButtons}>
+                <TouchableOpacity style={styles.buttonZero}
+                    onPress={() => {
+                        valuePress(0)
+                    }
+                    }
+                >
+                    <Text>0</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        valuePress('.')
+                    }
+                    }
+                >
+                    <Text>.</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        separa(value)
+                    }
+                    }
+                >
+                    <Text>=</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={styles.contentsButtons}>
-
-            <TouchableOpacity
-
-
-                onPress={() => {
-                    setValue('0')
-                }}
-
-                style={[styles.button, styles.buttonAC]}
-            >
-                <Text>AC</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress('/')
-                }
-                }
-            >
-                <Text>/</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.contentsButtons}>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(7)
-                }
-                }
-            >
-
-                <Text>7</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(8)
-                }
-                }
-            >
-                <Text>8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(9)
-                }
-                }
-            >
-                <Text>9</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress('*')
-                }
-                }
-            >
-                <Text>*</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.contentsButtons}>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(4)
-                }
-                }
-            >
-                <Text>4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(5)
-                }
-                }
-            >
-                <Text>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(6)
-                }
-                }
-            >
-                <Text>6</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress('-')
-                }
-                }
-            >
-                <Text>-</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.contentsButtons}>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(1)
-                }
-                }
-            >
-                <Text>1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(2)
-                }
-                }
-            >
-                <Text>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress(3)
-                }
-                }
-            >
-                <Text>3</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress('+')
-                }
-                }
-            >
-                <Text>+</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.contentsButtons}>
-            <TouchableOpacity style={styles.buttonZero}
-                onPress={() => {
-                    valuePress(0)
-                }
-                }
-            >
-                <Text>0</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    valuePress('.')
-                }
-                }
-            >
-                <Text>.</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}
-                onPress={() => {
-                    separa(value)
-                }
-                }
-            >
-                <Text>=</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-);
+    );
 }
 
 const styles = StyleSheet.create({
